9eb4940e9f9961cdaa0ebc46496756be9d1a763c
Stavros Tsioulis <contact@stavrostsioulis.com>
2024-11-04 15:43:52 +0200

    feat: add type-safe payload casting

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,2 +1,46 @@
 import * as sentinel from "./src/index.js"
 export default sentinel
+
+class BarkCollector extends sentinel.Collector<number> {
+  protected barks = 0
+  
+  protected getTimesBarked() {
+    return this.barks++
+  }
+
+  public getData(): number {
+    return this.getTimesBarked()
+  }
+}
+
+class AvailabilityStatusCollector extends sentinel.Collector<string> {
+  protected status: 'available' | 'unavailable' = 'available'
+  
+  protected getAvailability() {
+    return this.status
+  }
+
+  public getData(): string {
+    return this.getAvailability()
+  }
+}
+
+const barkWatchSource = new sentinel.SentinelWatchSource(
+  new sentinel.IntervalScheduler(1000),
+  new BarkCollector()
+)
+
+const availabilityStatusWatchSource = new sentinel.SentinelWatchSource(
+  new sentinel.IntervalScheduler(5000),
+  new AvailabilityStatusCollector()
+)
+
+const watcher = new sentinel.Sentinel([barkWatchSource, availabilityStatusWatchSource])
+
+watcher.on('watcherData', (payload) => {
+  if (sentinel.isPayloadFromSource(payload, barkWatchSource)) {
+    const [data] = payload.data
+  }
+})
+
+watcher.start()
--- packages/core/src/Sentinel/index.ts
+++ packages/core/src/Sentinel/index.ts
@@ -1,2 +1,13 @@
+import type { Collector } from "../Collector/Collector.js"
+import type { SentinelDataEventPayload } from "./Sentinel.js"
+import type { SentinelWatchSource } from "./SentinelWatchSource.js"
+
 export * from "./Sentinel.js"
 export * from "./SentinelWatchSource.js"
+
+export function isPayloadFromSource<WatchSource extends SentinelWatchSource<any, Collector<any>>>(
+  payload: SentinelDataEventPayload<any>,
+  watchSource: WatchSource,
+): payload is SentinelDataEventPayload<WatchSource> {
+  return payload.source === watchSource
+}
